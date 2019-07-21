638388aa419bae5a50f2d8f8e9469b7b95767e42
mchollar <NTP-rFD-GCQ-kL5>
2019-07-20 20:56:38 -0600

    Initial Commit

---
+++ CardDealer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardDealer
+//
+//  Created by Micah Chollar on 7/20/19.
+//  Copyright © 2019 Widgetilities. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
